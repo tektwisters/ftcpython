9fa12f0adbeeaed3eb93d506b366c491abde560a
tektwisters <tektwisters2.0@gmail.com>
2015-06-08 10:26:40 -0400

    Added Servos IrSeekerSensors and K9IrSeeker
    Added the K9IrSeeker example that was in the SDK

--- ftc/main.py
+++ ftc/main.py
@@ -50,6 +50,35 @@ class Motor (object):
     def setPower(self,value):
         tcpWrite('A,' + self.name + ',' + str(value))
         return int(tcpRead())
+        
+class Servo (object):
+    def __init__(self,name):
+        self.name = name
+        tcpWrite('E,' + self.name);
+    def setPosition(self,position):
+        tcpWrite('F,' + self.name + ',' + str(position))
+
+class IrSeekerSensor (object):
+    def __init__(self,name):
+        self.name = name
+        tcpWrite('G,' + self.name)
+        
+    def signalDetected(self):
+        tcpWrite('H,' + self.name)
+        result = tcpRead()
+        if result == "1":
+            return True
+        else:
+            return False
+            
+    def getAngle(self):
+        tcpWrite('I,' + self.name)
+        return float(tcpRead())
+        
+    def getStrength(self):
+        tcpWrite('J,' + self.name)
+        return float(tcpRead())
+        
 
 def running():
     tcpWrite('C')
@@ -73,7 +102,77 @@ class Demo(object):
     def loop(self):
         telemetry('2','Looped ' + str(self.count) + ' times')
         self.count += 1
-
+        
+class K9IrSeeker (object):
+    def __init__(self):
+        global MOTOR_POWER, HOLD_IR_SIGNAL_STRENGTH
+        MOTOR_POWER = 0.15
+        HOLD_IR_SIGNAL_STRENGTH = 0.50
+        
+    def start(self):
+        global motorRight
+        motorRight = Motor("motor_2")
+        
+        global motorLeft
+        motorLeft = Motor("motor_1",reverse = True)
+        
+        global arm
+        arm = Servo("servo_1")
+        
+        global claw
+        claw = Servo("servo_6")
+        
+        global armPosition
+        armPosition = 0.1
+        
+        global clawPosition
+        clawPosition = 0.25
+        
+        global irSeeker
+        irSeeker = IrSeekerSensor("ir_seeker")
+    
+    def loop(self):
+        angle = 0.0
+        strength = 0.0
+        left = 0.0
+        right = 0.0
+        
+        arm.setPosition(armPosition)
+        claw.setPosition(clawPosition)
+        
+        if irSeeker.signalDetected():
+            angle = irSeeker.getAngle()
+            strength = irSeeker.getStrength
+            if angle < -60:
+                left = -MOTOR_POWER
+                right = MOTOR_POWER
+            elif angle < -5:
+                left = MOTOR_POWER - 0.05
+                right = MOTOR_POWER
+            elif angle > 5 and angle < 60:
+                left = MOTOR_POWER
+                right = MOTOR_POWER - 0.05
+            elif angle > 60:
+                left = MOTOR_POWER
+                right = -MOTOR_POWER
+            elif strength < HOLD_IR_SIGNAL_STRENGTH:
+                left = MOTOR_POWER
+                right = MOTOR_POWER
+            else:
+                left = 0.0
+                right = 0.0
+        else:
+            left = 0.0
+            right = 0.0
+            
+        motorRight.setPower(right)
+        motorLeft.setPower(left)
+        
+        telemetry("Text", "*** Robot Data***")
+        telemetry("angle", "angle: " + str(angle))
+        telemetry("strength", "sig strength: " + str(strength))
+        telemetry("left tgt pwr", "left pwr: " + str(left))
+        telemetry("right tgt pwr", "right pwr: " + str(right))
 
 #State the currently selected class
 selected = Demo()
