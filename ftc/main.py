ad8c016dbaf16edba11596f2273a551b3f693145
Brandon Harris <tektwisters2.0@gmail.com>
2015-06-09 17:31:12 -0400

    Added error logging

--- ftc/main.py
+++ ftc/main.py
@@ -1,14 +1,40 @@
 from server import *
 from activity import *
 from misc import *
+import traceback, sys
 
+LOG_FILENAME = '/storage/emulated/0/ftc/log'
+
+def log(text):
+    with open(LOG_FILENAME, 'a') as f:
+        f.write(text + '\n')
+
+def log_error(e):
+    for frame in traceback.extract_tb(sys.exc_info()[2]):
+        fname,lineno,fn,text = frame
+        t = "Error in %s on line %d" % (fname, lineno)
+    log(t)
+    log(repr(e))
+
+log('\nStart of log\n')
+
+log('Starting server...')
 s = Server(1,25658)
+log('Listening for clients...')
 s.acceptClients()
+log('Connected')
 
-active = selected(s)
+try:
+    active = selected(s)
+except Exception as e:
+    log('Error:')
+    log_error(e)
 
-active.start()
-while running(s):
-    active.loop()
-
+try:
+    active.start()
+    while running(s):
+        active.loop()
+except Exception as e:
+    log('User code error:')
+    log_error(e)
 p("Stopped")
